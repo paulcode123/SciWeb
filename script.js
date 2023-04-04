d3eef70c70e63b3cbf2cd4d1baf1fe43d7111b03
c26pn <43388873+paulcode123@users.noreply.github.com>
2023-04-04 21:46:24 +0000

    Has home page, login page with cross linking, formatting

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
- document.getElementById('info.txt')
-            .addEventListener('change', function() {
-              
-            var fr=new FileReader();
-            fr.onload=function(){
-                document.getElementById('output')
-                        .textContent=fr.result;
-            }
-              
-            fr.readAsText(this.files[0]);
-        })
+ // Get the form element
+      const form = document.getElementById('login-form');
+
+      // Attach a submit event listener to the form
+      form.addEventListener('submit', function(event) {
+        // Prevent the form from submitting normally
+        event.preventDefault();
+
+        // Get the input values
+        const fname = document.getElementById('fname').value;
+        const lname = document.getElementById('lname').value;
+        const email = document.getElementById('email').value;
+        const grade = document.getElementById('grade').value;
+
+        // Create a CSV row from the input values
+        const csvRow = `${fname},${lname},${email},${grade}\n`;
+
+        // Create a new file writer
+        const fileWriter = new FileWriter('logins.csv');
+
+        // Write the CSV row to the file
+        fileWriter.write(csvRow);
+
+        // Alert the user that the data has been saved
+        alert('Your login information has been saved!');
+      });
