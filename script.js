40a2190987813d809e71b6e0f5f361b0d00e3449
c26pn <43388873+paulcode123@users.noreply.github.com>
2023-04-05 22:59:34 +0000

    Added use of a csv file to store user login input

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
  // Get the form element
+
       const form = document.getElementById('login-form');
 
       // Attach a submit event listener to the form
+      
       form.addEventListener('submit', function(event) {
+        
         // Prevent the form from submitting normally
         event.preventDefault();
-
+        
         // Get the input values
         const fname = document.getElementById('fname').value;
         const lname = document.getElementById('lname').value;
         const email = document.getElementById('email').value;
         const grade = document.getElementById('grade').value;
-
+        
         // Create a CSV row from the input values
         const csvRow = `${fname},${lname},${email},${grade}\n`;
 
-        // Create a new file writer
-        const fileWriter = new FileWriter('logins.csv');
-
-        // Write the CSV row to the file
-        fileWriter.write(csvRow);
-
+        // Create a Blob object
+        const blob = new Blob([csvRow], { type: 'text/csv' });
+        
+        const existingRows = localStorage.getItem('logins.csv');
+        alert(existingRows)
+          localStorage.setItem('logins.csv', existingRows + csvRow);
         // Alert the user that the data has been saved
-        alert('Your login information has been saved!');
+        alert("Your login information has been saved!");
       });
