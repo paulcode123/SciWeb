59d035a60032eb54b193e06cf1f9ca453cb454c1
Rking2442 <126593676+Rking2442@users.noreply.github.com>
2024-06-17 14:39:46 -0400

    Update review.js

--- templates/review.js
+++ templates/review.js
@@ -1,13 +1,13 @@
-<script> 
+
 document.getElementById('courseReviewSubmit').addEventListener('click', post_review); 
-fucntion post_review() { 
+function post_review() { 
 let workloadRating = document.getElementById('workRating').value; 
 let workloadReview = document.getElementById('workRating').value; 
 let enjoymentloadReview = document.getElementById('enjoymentReview').value; 
 let enjoymentloadRating = document.getElementById('enjoymentRating').value; 
 let diffcultyloadRating = document.getElementById('difficultyRating').value; 
 let difficultyReview = document.getElementById('difficultyReview').value; 
-
+}
 let course_id = window.location.href.slice(-5); 
 //ChatGpt Code for last 5 window letters
 
@@ -19,7 +19,8 @@ let data = {
     Enjoyment_Rating: enjoymentloadRating,
     Enjoyment_Review: enjoymentloadReview,
     Course-Id: course_id
-};
+} 
+
 fetchRequest('/post_review', {data: data, sheets: 'Reviews'}); 
 
 
@@ -31,4 +32,3 @@ fetchRequest('/post_review', {data: data, sheets: 'Reviews'});
 
 
 
-    </script> 
