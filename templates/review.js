f2ed162f30119c678af669cf7e222bac872f1b5f
Paul Nieuwerburgh <nieuwerbp@bxscience.edu>
2024-06-18 16:28:59 -0400

    small fixes

--- templates/review.js
+++ templates/review.js
@@ -2,29 +2,26 @@
 document.getElementById('courseReviewSubmit').addEventListener('click', post_review); 
 function post_review() { 
 let workloadRating = document.getElementById('workRating').value; 
-let workloadReview = document.getElementById('workRating').value; 
+let workloadReview = document.getElementById('workReview').value; 
 let enjoymentloadReview = document.getElementById('enjoymentReview').value; 
 let enjoymentloadRating = document.getElementById('enjoymentRating').value; 
-let diffcultyloadRating = document.getElementById('difficultyRating').value; 
+let difficultyRating = document.getElementById('difficultyRating').value; 
 let difficultyReview = document.getElementById('difficultyReview').value; 
-}
+
 let course_id = window.location.href.slice(-5); 
 //ChatGpt Code for last 5 window letters
 
 let data = {
-    Workload-Rating: workloadRating,
-    Workload_Review: workloadReview,
-    Difficulty_Rating: difficultyloadRating,
-    Difficulty_Review: difficultyloadReview,
-    Enjoyment_Rating: enjoymentloadRating,
-    Enjoyment_Review: enjoymentloadReview,
-    Course-Id: course_id
+    WorkLoadRating: workloadRating,
+    WorkLoadReview: workloadReview,
+    DifficultyRating: difficultyRating,
+    DifficultyReview: difficultyReview,
+    EnjoymentRating: enjoymentloadRating,
+    EnjoymentReview: enjoymentloadReview,
+    CourseId: course_id
 } 
 
-fetchRequest('/post_review', {data: data, sheets: 'Reviews'}); 
-
-
-
+fetchRequest('/post_data', {data: data, sheet: 'Reviews'}); 
 
 }
 
